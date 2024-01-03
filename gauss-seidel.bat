5de87a99dda1ea2a8a80948f6dce9ad49ae89531
Nicolo' Vittorio Marino <20044867@studenti.uniupo.it>
2024-01-03 22:50:18 +0100

    Metodo iterativo di gauss-seidel

--- gauss-seidel.bat
+++ gauss-seidel.bat
@@ -2,4 +2,8 @@ gcc gauss-seidel.c -o gauss-seidel -Wall -Wextra -Wconversion
 
 gauss-seidel sistemi\%1
 
-gnuplot -e "error_per_iteration='gnuplot\%1'; set term png; set output 'gnuplot\%1.png'; plot error_per_iteration with lines"
+set "gnuplot_script=gnuplot\%1"
+
+if exist %gnuplot_script% (
+    gnuplot -e "error_per_iteration='%gnuplot_script%'; set term png; set output 'gnuplot\%1.png'; plot error_per_iteration with lines"
+)
